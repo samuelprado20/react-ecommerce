0a7017862e97df62131a561f7f347462ec015e83
Samuel Prado <samuel.prado.osorio20@gmail.com>
2023-08-13 00:00:27 -0500

    Add routes

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,3 +1,4 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrders from '../MyOrders'
@@ -6,16 +7,24 @@ import NotFound from '../NotFound'
 import SignIn from '../SingIn'
 import './App.css'
 
+const AppRoutes = () => {
+  const routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/sign-in', element: <SignIn/> },
+    { path: '/*', element: <NotFound /> }
+  ])
+
+  return routes
+}
+
 function App () {
   return (
-    <>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
